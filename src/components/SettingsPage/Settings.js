9c5c8d1085c8dd085d1612f12b49fcdbf4ebd586
Tahsin Abir <tahsinul.h.abir@vanderbilt.edu>
2022-03-26 23:52:08 -0500

    Added instructions

--- src/components/SettingsPage/Settings.js
+++ src/components/SettingsPage/Settings.js
@@ -1,10 +1,30 @@
-import HomeBtn from "../HomePage/HomeBtn"
+import ListGroup from 'react-bootstrap/ListGroup'
+import HomeBtn from '../HomePage/HomeBtn'
 
 const Settings = () => {
   return (
     <div>
+      <div className="results-header">
+        Instructions
+      </div>
+      <div className="instructions">
+        1. Pressing the quiz button will start a new quiz every time
+        <br></br>
+        <br></br>
+        2. Show progress shows the quiz performance over a long period of time
+        <br></br>
+        <br></br>
+        3. The flashcards button will show a series of flashcards
+        <br></br>
+        <br></br>
+        4. The Games button have links to different games beneficial to people with dementia
+        <br></br>
+        <br></br>
+      </div>
+      
+
       <HomeBtn css="btn-home"/>
-    </div>
+  </div>
   )
 }
 
